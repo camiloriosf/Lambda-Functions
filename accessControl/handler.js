fe499da4a939259eae3c524b89d649f719342d70
camiloriosf <camilo.rios.f@gmail.com>
2017-11-07 12:52:46 -0300

    added function

--- accessControl/handler.js
+++ accessControl/handler.js
@@ -3,13 +3,13 @@
 const config = require('./config');
 const mailgun = require('mailgun-js')({ apiKey: config.api_key, domain: config.domain });
 
-function send(name, email) {
+function send(name, email, subject, text) {
   const data = {
     from: 'Bittersweet.io <contact@bittersweet.io>',
     to: email,
     bcc: 'camilo.rios@bittersweet.io',
-    subject: 'Cuenta Creada',
-    text: `Hola ${name},\n\nHemos creado tu cuenta, puedes ingresar haciendo click en el siguiente enlace\n\nhttps://ac.bittersweet.io/admin`
+    subject: subject,
+    text: text
   };
 
   mailgun.messages().send(data, function (error, body) {
@@ -27,7 +27,32 @@ module.exports.sendConfirmationEmail = (event, context, callback) => {
     }),
   };
 
-  send(body.payload.changedUser.name, body.payload.changedUser.username);
+  const name = body.payload.changedUser.name;
+  const username = body.payload.changedUser.username;
+  const subject = 'Cuenta Creada';
+  const text = `Hola ${name},\n\nHemos creado tu cuenta, puedes ingresar haciendo click en el siguiente enlace\n\nhttps://ac.bittersweet.io/admin`;
+
+  send(name, username, subject, text);
+
+  callback(null, response);
+};
+
+module.exports.sendContactConfirmationEmail = (event, context, callback) => {
+  const body = JSON.parse(event.body);
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: body,
+    }),
+  };
+
+  const name = body.payload.changedContact.name;
+  const email = body.payload.changedContact.email;
+  const subject = 'Mensaje recibido';
+  const text = `Hola ${name},\n\nHemos recibido tu mensaje, nos pondremos en contacto a la brevedad`;
+
+  send(name, email, subject, text);
 
   callback(null, response);
 };
